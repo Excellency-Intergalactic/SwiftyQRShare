3776ac72c7ba64cfed96c69c5471dca855f40d14
CEOSPEEDBIRD <ceo.uz@gmx.de>
2022-05-07 17:43:05 +0200

    Initial Commit

---
+++ Sources/SwiftyQRShare/SwiftyQRShare.swift
@@ -0,0 +1,6 @@
+public struct SwiftyQRShare {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftyQRShareTests/SwiftyQRShareTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyQRShare
+
+final class SwiftyQRShareTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyQRShare().text, "Hello, World!")
+    }
+}
