1bae307f87460992bf00bfa89937c0eb04bc2858
CEOSPEEDBIRD <ceo.uz@gmx.de>
2022-06-19 10:59:50 +0200

    Updated README

--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 ### A [Swift](https://swift.org) framework for *sharing Data easily with QR Codes* on iOS and iPadOS with [SwiftUI](https://developer.apple.com/xcode/swiftui/).
 
 ## How to use
+Your Data Model must conform to Codable
+
+### Generate a QR Code:
+
+```swift
+// Generate UIImage: 
+getQRCode(from: CodableStruct)
+// Generate SwiftUI Image: 
+getQRCodeImage(from: CodableStruct)
+```
+
+
+### Retrieve Data:
+Use a Swift UI View
+
+```swift
+struct MyView: View {
+    @State var data = 0
+    @State var showScanView = false
+    var body: some View {
+        Button("Scan Code") {
+            showScanView.toggle()
+        }.sheet(isPresented: $showScanView) {
+            DataScanView(value: $data)
+        }
+    }
+}
+```
--- Sources/SwiftyQRShare/SwiftyQRShare.swift
+++ Sources/SwiftyQRShare/SwiftyQRShare.swift
@@ -1,19 +1,18 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-public struct QRShare {
     func getQRCode<T: Codable>(from: T) -> UIImage {
         let encoder = JSONEncoder()
         let encodedExam = try? encoder.encode(from)
         return generateQRCode(from: String(decoding: encodedExam ?? Foundation.Data(), as: UTF8.self))
     }
+
+@available(iOS 13.0, *)
     func getQRCodeImage<T: Codable>(from: T) -> Image {
         let encoder = JSONEncoder()
         let encodedExam = try? encoder.encode(from)
         return Image(uiImage: generateQRCode(from: String(decoding: encodedExam ?? Foundation.Data(), as: UTF8.self)))
     }
-    
-}
 
 @available(iOS 13.0, *)
 func importObject<T: Codable>(data: Data) -> T? {
@@ -21,3 +20,5 @@ func importObject<T: Codable>(data: Data) -> T? {
     let item = try? decoder.decode(T.self, from: data)
     return item
 }
+
+
